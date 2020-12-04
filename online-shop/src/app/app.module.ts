07ef35b664684d3c1189325218e6b5826cc8140b
Stefan Morar <stefan.morar30@gmail.com>
2020-12-04 14:37:53 +0200

    #OPT-1 Install Material Design and replace HTML buttons with material buttons

--- online-shop/src/app/app.module.ts
+++ online-shop/src/app/app.module.ts
@@ -7,10 +7,25 @@ import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, ProductDetailsComponent, ProductListComponent, ShoppingCartComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    ShoppingCartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
